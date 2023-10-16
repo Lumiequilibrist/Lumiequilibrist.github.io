b7124f8a17ae3afdb78601498594ff61eb5a0614
Lumiequilibrist <1215743100@qq.com>
2023-10-16 23:18:16 +0800

    Site updated: 2023-10-16 23:18:16

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -1,6 +1,16 @@
 let themeFunc = async function() {
     let btn = document.getElementById("theme-btn");
     let root = document.documentElement;
+    
+    // 在页面加载时，从localStorage中获取主题
+    let storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        root.setAttribute('theme', storedTheme);
+    } else {
+        // 如果localStorage中没有主题，则默认设置为亮色模式
+        root.setAttribute('theme', 'light');
+    }
+    
     btn.addEventListener('click', e => {
         if (root.getAttribute('theme') == 'dark') {
             root.setAttribute('theme', 'light');
@@ -12,4 +22,3 @@ let themeFunc = async function() {
     });
 };
 themeFunc();
-
